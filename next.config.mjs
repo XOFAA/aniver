eec3f605543a5b49111e72d04bb2dcc462961cfb
XOFAA <giovanne.oliv@gmail.com>
2026-01-04 03:22:52 -0400

    configuracao

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-
-  images: {
-    unoptimized: true,
-  },
-
-  // opcional: garante trailing slash (bom pra static host)
   trailingSlash: true,
+  images: { unoptimized: true },
+  typescript: { ignoreBuildErrors: true },
 }
 
 export default nextConfig
