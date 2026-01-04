ff007720749c494916e85c04d57db17e246bfe41
XOFAA <giovanne.oliv@gmail.com>
2026-01-04 03:14:21 -0400

    correcao

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
   },
+
+  // opcional: garante trailing slash (bom pra static host)
+  trailingSlash: true,
 }
 
 export default nextConfig
